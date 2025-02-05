4813e8d9372aeab43390a166aaaa160115a2a271
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-05 17:51:47 -0600

    add routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<header>Headie</header>
+
+	<main>
+		{@render children()}
+	</main>
+
+	<footer>
+		<p>Footie</p>
+	</footer>
+</div>
+
+<style>
+	@import './styles.css';
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Boncurs Tools</title>
+	<meta
+		name="A collection of tools to use with the Boncurs Bonding Curve Library"
+		content="Boncurs Tools"
+	/>
+</svelte:head>
+
+<section>
+	<h1>Welcome to the Body</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
