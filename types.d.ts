8eadbe460a086e7b23a00b2396166f30198f2dda
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-12 13:12:32 -0600

    update global types

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,18 @@
 // Define the structure of the form data
 export type FormData = {
 	selectedCurve: string;
-	initialReserveBalance: number;
+	reserveBalance: number;
 	reserveRatio: number;
 	initialCost: number;
 	scalingFactor: number;
 };
+
+export type ConvertedFormData = {
+	selectedCurve: string;
+	reserveBalance: bigint;
+	reserveRatio: bigint;
+	initialCost: bigint;
+	scalingFactor: bigint;
+};
+
+export type TableData = { tokens: number[]; purchaseCosts: bigint[]; saleReturns: bigint[] };
