7094700f96b1bfbe088255a4e9d4af19ef550f35
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-16 22:56:44 -0600

    Add Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<header>
+	<h1>Boncurs CurveLab</h1>
+	<a href="https://github.com/dustinstacy/boncurs-toolkit" target="_blank">
+		<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub logo" />
+	</a>
+</header>
+
+<style>
+	@import './Header.css';
+</style>
